b6367f0842bdff1ddaf8f2a280c37de254e029e9
blackcatdev <marcinstarzyk1@gmail.com>
2019-09-24 12:29:56 +0200

    + homework3: program throws exception :>

---
+++ src/pl/blackcat/kursjava/division/unboxingException.java
@@ -0,0 +1,11 @@
+package pl.blackcat.kursjava.division;
+
+//Write a program that throws an exception during unboxing / boxing.
+
+public class unboxingException {
+	public static void main(String[] args) {
+		//throws NumberFormatException as expected. :>
+		int Value1 = Integer.valueOf("s");
+
+	}
+}
